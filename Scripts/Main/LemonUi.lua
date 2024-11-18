cfe6b3aaa4b3746a410b10b2a24445caa6729fb8
Baodzvip <116901121+Baodzvip@users.noreply.github.com>
2024-11-18 22:41:05 +0700

    Update LemonUi.lua

--- Scripts/Main/LemonUi.lua
+++ Scripts/Main/LemonUi.lua
@@ -16,7 +16,7 @@ local Window = Fluent:CreateWindow({
 local Tabs = {
     Main = Window:AddTab({ Title = "Main", Icon = "airplay" }),
     AutoFarm = Window:AddTab({ Title = "Farm", Icon = "citrus" }),
-    ChooseWeapon = Window:AddTab({ Title = "Choose Weapon", Icon = "Swords" }),
+    ChooseWeapon = Window:AddTab({ Title = "Choose Weapon", Icon = "sword" }),
     Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
 }
 
