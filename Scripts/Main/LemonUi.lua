7dfff5549ce41c278780a6441f38793413cc5cbe
Baodzvip <116901121+Baodzvip@users.noreply.github.com>
2024-11-22 01:17:58 +0700

    Update LemonUi.lua

--- Scripts/Main/LemonUi.lua
+++ Scripts/Main/LemonUi.lua
@@ -75,10 +75,10 @@ do
 
             
     local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "KillAura", Default = false })
-    Toggle.OnChanged:(function(value)
+    Toggle.OnChanged:(function()
     loadstring(game:httpGet("https://raw.githubusercontent.com/Baodzvip/Free/refs/heads/main/Scripts/LemonKillaura.lua"))()
-            end)
-    Options.MyToggle:SetValue(false)
+    Options.MyToggle:SetValue(true)
+        end)
 
 
     
