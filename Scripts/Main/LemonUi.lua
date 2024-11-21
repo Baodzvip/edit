7e0666ad61bbb3ebc75547b4c7fe3e0bc3262b42
Baodzvip <116901121+Baodzvip@users.noreply.github.com>
2024-11-22 00:51:17 +0700

    Update LemonUi.lua

--- Scripts/Main/LemonUi.lua
+++ Scripts/Main/LemonUi.lua
@@ -75,10 +75,9 @@ do
 
             
     local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "KillAura", Default = false })
-    local value = true
     Toggle.OnChanged:Connect(function()
     getgenv().KillAURA = true
-  while getgenv().KillAURA do
+    while getgenv().KillAURA do
     task.wait()
        sethiddenproperty(game.Players.LocalPlayer, "SimulationRadius", 200)
           sethiddenproperty(game.Players.LocalPlayer, "MaxSimulationRadius", 200)
