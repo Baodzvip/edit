7fd844585d2cd67e5c183e2e311ccbce0475cac9
Baodzvip <116901121+Baodzvip@users.noreply.github.com>
2024-11-21 23:18:20 +0700

    Update LemonUi.lua

--- Scripts/Main/LemonUi.lua
+++ Scripts/Main/LemonUi.lua
@@ -235,20 +235,6 @@ Tabs.AutoFarm:AddParagraph({
         Content = "Best Farm"
     })
 
-local Toggle = Tabs.AutoFarm:AddToggle("MyToggle", {Title = "KillAura", Default = false })
-
-    Toggle:OnChanged(function()
-        _G.KillAura = true
-        while KillAura do wait()
-            pcall(function()
-                for i,v in pairs(game:GetService("Workspace").Enemies:GetDescendants()) do
-                    if v.ClassName == "Model" and v.Humanoid.Health > 0 then
-                        v.Humanoid.Health = Die
-                        sethiddenproperty(game.Players.LocalPlayer, "SimulationRadius",  200)
-                    end
-                end
-            end)
-    Options.MyToggle:SetValue(false)
 
 
 Fluent:Notify({
