63c90ffc2d0248d819fa0614a25720746e055131
Baodzvip <116901121+Baodzvip@users.noreply.github.com>
2024-11-22 00:10:33 +0700

    Update LemonUi.lua

--- Scripts/Main/LemonUi.lua
+++ Scripts/Main/LemonUi.lua
@@ -76,18 +76,7 @@ do
             
     local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "KillAura", Default = false })
     Toggle:OnChanged(function()
-    while getgenv().KillAURA do
-    task.wait()
-       sethiddenproperty(game.Players.LocalPlayer, "SimulationRadius", 200)
-          sethiddenproperty(game.Players.LocalPlayer, "MaxSimulationRadius", 200)
-              for _, v in pairs(game:GetService("Workspace"):GetDescendants()) do
-                if v.ClassName == 'Humanoid' and v.Parent.Name ~= game.Players.LocalPlayer.Name then
-             v.Health = 0
-          end
-       end
-    end
-end)
-    end)
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Baodzvip/Free/refs/heads/main/Scripts/LemonKillaura.lua"))()
     Options.MyToggle:SetValue(false)
 
 
